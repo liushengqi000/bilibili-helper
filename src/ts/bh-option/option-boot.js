a3db8c551f6b71e79cf339810f4d7d2299a827f2
Ruo <jjj201200@gmail.com>
2017-06-17 04:16:30 +0800

    clean up files

--- src/ts/bh-option/option-boot.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-var bh_option_module_1 = require("../bh-option/bh-option.module");
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(bh_option_module_1.BhOptionModule);
-//# sourceMappingURL=option-boot.js.map
